ec124a89c93b2e25f76174d4d26e222f1438d20d
Ashley <adunderwood96@gmail.com>
2021-11-17 21:13:33 -0500

    added questions while working on html setup

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+
+// Questions/Answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is NOT included in data types?",
+        answer: "alerts",
+        options: [
+            "Strings",
+            "Alerts",
+            "Booleans",
+            "Numbers",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is the HTML attribute used when an image does not appear?",
+        answer: "alt",
+        options: [
+            "src",
+            "alt",
+            "title",
+            "image",
+        ]
+    },
+    {
+        numb: 5,
+        question: "How do you write a function in JavaScript?",
+        answer: "function myFunction()",
+        options: [
+            "function myFunction()",
+            "function = myFunction()",
+            "function:myFunction()",
+            "myfunction =()",
+        ]
+    },
+];
