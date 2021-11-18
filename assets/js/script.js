1b31717eae66c55b045dc0f0f535ff0c1ec05026
Ashley <adunderwood96@gmail.com>
2021-11-18 16:46:39 -0500

    adding variables for quiz into JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Variables
+
+// Timer
+const timeE1 = document.querySelector('#time')
+const timeLeft = 60;
+const timer;
+
+// Start Quiz
+const startBtn = document.querySelector('#start');
+const startPrompt = document.querySelector('#start-prompt');
+
+// Q & A
+const questions = document.querySelector('#questions');
+const answers = document.querySelector('#answers');
+
+// User Score
+
+// HighScore
+
