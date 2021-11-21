ae012981e9b15b3ea0f3a4ec06c304b7fe23f81c
Ashley <adunderwood96@gmail.com>
2021-11-20 19:01:49 -0500

    added more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -67,5 +67,53 @@ let questions = [
             "<h1>",
             "<head>",
         ]
-    }
+    },
+
+    {
+        numb: 7,
+        question: "What is the correct way to add a line break into HTML?",
+        answer: "<br>",
+        options: [
+            "<br>",
+            "<break>",
+            "<lb>",
+            "<line>",
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "What is the proper way to add a comment in JS?",
+        answer: "// comment",
+        options: [
+            "--!comment--",
+            "'comment'",
+            "!comment",
+            "// comment",
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "What is the largest HTML element for headings?",
+        answer: "<h1>",
+        options: [
+            "<heading>",
+            "<h6>",
+            "<h1>",
+            "<head>",
+        ]
+    },
+    
+    {
+        numb: 10,
+        question: "In CSS how do you select an id element?",
+        answer: "#id",
+        options: [
+            ".id",
+            "!id",
+            "#id",
+            "*id",
+        ]
+    },
 ];
