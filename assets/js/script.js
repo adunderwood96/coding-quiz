f41c2dfceb0046d364d11bff0a6dc54a42340b2c
Ashley <adunderwood96@gmail.com>
2021-11-23 18:02:05 -0500

    got quiz questions to progress, however all choices when click say incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,147 +1,63 @@
-const timeEl = document.querySelector('#time');
-let timeLeft = 90;
-let timerId;
+var timeEl = document.querySelector('#time');
+var timeLeft = 90;
+var timerId;
 
 // Start block
-const startBtn = document.querySelector('#start');
-const startPage = document.querySelector('#start-page');
+var startBtn = document.querySelector('#start');
+var startPage = document.querySelector('#start-page');
 
 // Question & answers block
-const quizBox = document.querySelector('#quiz-box');
-const questionText = document.querySelector('#question-text');
-const choicesEl = document.querySelector('#choices');
-const resultEl = document.querySelector('#result')
+var quizBox = document.querySelector('#quiz-box');
+var questionText = document.querySelector('#question-text');
+var choicesEl = document.querySelector('#choices');
+var resultEl = document.querySelector('#result')
 
 // User score block
-const scoreBox = document.querySelector('#end-page');
-const scoreEl = document.querySelector('#score');
-const submitScore = document.querySelector('#submit');
-const inputEl = document.querySelector('#input');
-const initials = document.querySelector("#initials");
+var scoreBox = document.querySelector('#end-page');
+var scoreEl = document.querySelector('#score');
+var submitScore = document.querySelector('#submit');
+var inputEl = document.querySelector('#input');
+var initials = document.querySelector("#initials");
 
-const questions = [
+var questions = [
     {
         text: "What does HTML stand for?",
-        choices: [
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multiple Language",
-            "Hyper Tool Multi Language",
-        ],
-        answerIndex: 2,
-
+        choices: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"],
+        answerIndex: "Hyper Text Markup Language"
     },
 
     {
         text: "What does CSS stand for?",
-        choices: [
-            "Common Style Sheet",
-            "Colorful Style Sheet",
-            "Computer Style Sheet",
-            "Cascading Style Sheet",
-        ],
-        answerIndex: 4,
+        choices: ["Common Style Sheet", "Colorful Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"],
+        answerIndex: "Cascading Style Sheet"
 
     },
 
     {
         text: "What is NOT included in data types?",
-        choices: [
-            "Strings",
-            "Alerts",
-            "Booleans",
-            "indexNumberers",
-        ],
-        answerIndex: 2,
+        choices: ["Strings", "Alerts", "Booleans", "Numbers"],
+        answerIndex: "Numbers"
     },
 
     {
         text: "Which of the following is the HTML attribute used when an image does not appear?",
-        choices: [
-            "src",
-            "alt",
-            "text",
-            "image",
-        ],
-        answerIndex: 2,
+        choices: ["src", "alt", "text", "image"],
+        answerIndex: "alt"
     },
 
     {
         text: "How do you write a function in JavaScript?",
-        choices: [
-            "function myFunction()",
-            "function = myFunction()",
-            "function:myFunction()",
-            "myfunction =()",
-        ],
-        answerIndex: 1,
+        choices: ["function myFunction()", "function = myFunction()", "function:myFunction()", "myfunction =()"],
+        answerIndex: "function myFunction()"
     },
 
-    {
-        text: "What is the largest HTML element for headings?",
-        choices: [
-            "<heading>",
-            "<h6>",
-            "<h1>",
-            "<head>",
-        ],
-        answerIndex: 3,
-    },
-
-    {
-        text: "What is the correct way to add a line break into HTML?",
-        choices: [
-            "<br>",
-            "<break>",
-            "<lb>",
-            "<line>",
-        ],
-        answerIndex: 1,
-    },
-
-    {
-        text: "What is the proper way to add a comment in JS?",
-        choices: [
-            "--!comment--",
-            "'comment'",
-            "!comment",
-            "// comment",
-        ],
-        answerIndex: 4,
-    },
-
-    {
-        text: "What is the smallest HTML element for headings?",
-        choices: [
-            "<heading>",
-            "<h6>",
-            "<h1>",
-            "<head>",
-        ],
-        answerIndex: 2,
-    },
-
-    {
-        text: "In CSS how do you select an id element?",
-        choices: [
-            ".id",
-            "!id",
-            "id",
-            "*id",
-        ],
-        answerIndex: 3,
-    },
 ];
 
-let questionIndex = 0;
+var questionIndex = 0;
 
-// Event Listeners 
-startBtn.addEventListener('click', startClick);
-choicesEl.addEventListener('click', choicesClick);
-submitScore.addEventListener('click', submitClick);
-clearScore.addEventListener('click', clearClick);
+// Start Quiz
 
-function startClick() {
+startBtn.addEventListener('click', function startClick(e) {
     // Hide start page
     startPage.style.display = 'none';
 
@@ -152,7 +68,7 @@ function startClick() {
     quizBox.style.display = 'block';
 
     startQuiz();
-};
+});
 
 function countDown() {
     // Start Countdown timer for quiz
@@ -169,7 +85,7 @@ function countDown() {
 };
 
 function startQuiz() {
-    const currentQuestion = questions[questionIndex];
+    var currentQuestion = questions[questionIndex];
 
     questionText.textContent = currentQuestion.text;
 
@@ -178,11 +94,11 @@ function startQuiz() {
     //clear prev results
     clearResults();
 
-    for (let i = 0; i < currentQuestion.choices.length; i++) {
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
         // store the answer text
-        const answer = currentQuestion.choices[i];
+        var answer = currentQuestion.choices[i];
         // Create a button for each answer
-        const btn = document.createElement('button');
+        var btn = document.createElement('button');
 
         btn.setAttribute('class', 'btn');
         // Set the button text to the answers text
@@ -192,19 +108,17 @@ function startQuiz() {
     };
 };
 
-function choicesClick() {
-    e.answerpreventDefault();
+choicesEl.addEventListener('click', function choicesClick(e) {
+    e.preventDefault();
     if (!e.target.matches('button')) return;
 
-    // Did the user chose the correct answer?
-    // Store the user's answer
-    const userAnswer = e.target.textContent;
+    var userAnswer = e.target.textContent;
 
     // Retrieve current question
-    const question = questions[questionIndex];
+    var question = questions[questionIndex];
 
     // Get correct answer
-    const correct = question.choices[question.answerIndex];
+    var correct = question.choices[question.answerIndex];
 
     // Compare correct answer to user's response
     if (userAnswer === correct) {
@@ -223,7 +137,7 @@ function choicesClick() {
     }
 
     setTimeout(startQuiz, 1000);
-};
+});
 
 function displayCorrect() {
     // display correct answer div
@@ -241,7 +155,7 @@ function clearResults() {
     resultEl.style.display = 'none';
 }
 
-function displayScore() {    
+function displayScore() {
     // Hide everything
     quizBox.style.display = 'none';
     timeEl.style.display = 'none';
@@ -254,51 +168,8 @@ function displayScore() {
         scoreEl.textContent = 'Your score is 0'
     }
     else {
-    scoreEl.textContent = `Your score is ${timeLeft}`;
+        scoreEl.textContent = `Your score is ${timeLeft}`;
     }
-    
+
 
 };
-
-function submitClick(e) {
-  e.preventDefault();
-  // Allow user to record score
-    const user = {
-        initials: initials.value.trim(),
-        score: timeLeft,
-    };
-
-    
-    const scores = JSON.parse(localStorage.getItem('highscores')) || [];
-    scores.push(user);
-
-    // Create user on local storage 
-    localStorage.setItem('highscores', JSON.stringify(scores));
-
-    renderHighScore(user);
-    
-    // Save initials and score to localStorage
-
-  };
-
-  function renderHighScore(user) {
-    // Hide score section
-    scoreBox.style.display = 'none';
-  
-    // Show high score block
-    hscoreBox.style.display = 'block';
-    
-    // Display user's initial and score
-    highscoreEl.textContent = user.initials + ': ' + user.score;
-    
-      const highScoreList = localStorage.getItem('highscores');
-      highScoreList = JSON.parse(highScoreList);
-
-      };
-  
-
-  
-      function clearClick() {
-          // Clear score
-          highscoreEl.textContent = '';
-      }
