877dbb406dbd7890f410488302ff4e22ca2a39e4
Ashley <adunderwood96@gmail.com>
2021-11-26 12:17:12 -0500

    final updates

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,27 +2,27 @@ function printHighscores() {
 
   var highscores = JSON.parse(localStorage.getItem("highscores"));
 
-    // for each score
-    for (var i = 0; i < highscores.length; i++) {
-      // create list for score
-      var scoreLi = document.createElement("li");
-      scoreLi.textContent = highscores[i].initials + ": " + highscores[i].highscore;
-      // display on page
-      document.getElementById("highscores").appendChild(scoreLi);
-    }
+  // for each score
+  for (var i = 0; i < highscores.length; i++) {
+    // create list for score
+    var scoreLi = document.createElement("li");
+    scoreLi.textContent = highscores[i].initials + ": " + highscores[i].highscore;
+    // display on page
+    document.getElementById("highscores").appendChild(scoreLi);
   }
+}
 
 
-  function clearHighscores() {
-    // (and reload)
-    localStorage.removeItem("highscores");
-    location.reload();
-  }
+function clearHighscores() {
+  // (and reload)
+  localStorage.removeItem("highscores");
+  location.reload();
+}
 
-  // attach clear event to clear score button
-  var resetBtn = document.getElementById("reset");
-  resetBtn.addEventListener("click", function () {
-    clearHighscores();
-  })
-  // run printhighscore when page loads
-  printHighscores();
+// attach clear event to clear score button
+var resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", function () {
+  clearHighscores();
+})
+// run printhighscore when page loads
+printHighscores();
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,20 @@ var timeLeft = 60;
 var timerId;
 
 // Start section
-var startBtn = document.querySelector('#start');
+var startBtn = document.querySelector('#start-btn');
 var startPage = document.querySelector('#start-page');
 
 // Question & answers 
+var questionIndex = 0;
 var quizBox = document.querySelector('#quiz-box');
-var questionText = document.querySelector('#question-text');
+var questionTextEl = document.querySelector('#question-text');
 var choicesEl = document.querySelector('#choices');
-var resultEl = document.querySelector('#result')
+var resultEl = document.querySelector('#result');
 
 // User score input
 var scoreBox = document.querySelector('#end-page');
 var scoreEl = document.querySelector('#score');
-var submitScore = document.querySelector('#submit');
+var submitBtn = document.querySelector('#submit-btn');
 var inputEl = document.querySelector('#user-input');
 
 var questions = [
@@ -54,11 +55,10 @@ var questions = [
     }
 ];
 
-var questionIndex = 0;
 
 // Start Quiz
 
-startBtn.addEventListener('click', function handleStartClick(e) {
+startBtn.addEventListener('click', function handleStartBtnClick(e) {
     // Hide start page
     startPage.style.display = 'none';
 
@@ -86,13 +86,14 @@ function countDown() {
 };
 
 function startQuiz() {
+    // question shown
     var currentQuestion = questions[questionIndex];
 
-    questionText.textContent = currentQuestion.text;
+    questionTextEl.textContent = currentQuestion.text;
 
     choicesEl.innerHTML = '';
 
-    //clear prev results
+    //clear previous q
     clearResults();
 
     for (var i = 0; i < currentQuestion.choices.length; i++) {
@@ -107,8 +108,8 @@ function startQuiz() {
 choicesEl.addEventListener('click', function handleChoicesClick(e) {
     e.preventDefault();
     if (!e.target.matches('button'))
-    return;
-
+        return;
+// user response
     var userAnswer = e.target.textContent;
 
     //  current question
@@ -119,14 +120,15 @@ choicesEl.addEventListener('click', function handleChoicesClick(e) {
 
     //user response
     if (userAnswer === correct) {
+        // reward the user :)
         timeLeft += 3;
-        resultEl.style.display ="block";
-        resultEl.textContent ="Correct!";
+        resultEl.style.display = "block";
+        resultEl.textContent = "Correct!";
     }
     else {
-        // If incorrect penalize user by 10 seconds from time, and move to next question.
+        // If incorrect penalize user by 10 seconds from time
         timeLeft -= 10;
-        resultEl.style.display ="block";
+        resultEl.style.display = "block";
         resultEl.textContent = "Incorrect";
     }
 
@@ -150,7 +152,7 @@ function displayScore() {
     quizBox.style.display = 'none';
     timeEl.style.display = 'none';
 
-    // Show score 
+    // Show user score 
     scoreBox.style.display = 'block';
 
     if (timeLeft < 0) {
@@ -162,7 +164,7 @@ function displayScore() {
 
 };
 
-submitScore.addEventListener('click', function handleSaveHighscore(e) {
+submitBtn.addEventListener('click', function handleSaveHighscore(e) {
     e.preventDefault();
     // get value of input box
     var initials = inputEl.value.trim();
